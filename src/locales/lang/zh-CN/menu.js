0d58d58b2476c6a7ee9dd03cb127c13563d34588
lihh1300308284 <1300308284@qq.com>
2022-03-16 21:49:39 +0800

    add zjlist 翻译字典中文

--- src/locales/lang/zh-CN/menu.js
+++ src/locales/lang/zh-CN/menu.js
@@ -12,8 +12,10 @@ export default {
     'menu.form.step-form.confirm': '分步表单（确认转账信息）',
     'menu.form.step-form.result': '分步表单（完成）',
     'menu.form.advanced-form': '高级表单',
-    'menu.list': '列表页',
-    'menu.list.table-list': '查询表格',
+    // 'menu.list': '列表页',
+    'menu.list': '托管组邮件规则管理',
+    // 'menu.list.table-list': '查询表格',
+    'menu.list.table-list': '交易文件规则',
     'menu.list.basic-list': '标准列表',
     'menu.list.card-list': '卡片列表',
     'menu.list.search-list': '搜索列表',
@@ -35,5 +37,13 @@ export default {
     'menu.account.center': '个人中心',
     'menu.account.settings': '个人设置',
     'menu.account.trigger': '触发报错',
-    'menu.account.logout': '退出登录'
+    'menu.account.logout': '退出登录',
+    'menu.zj.list.list': '托管组邮件规则管理',
+    'menu.zj.list.table-list': '交易文件规则',
+    'menu.zj.list.basic-list': '标准列表',
+    'menu.zj.list.card-list': '卡片列表',
+    'menu.zj.list.search-list': '搜索列表',
+    'menu.zj.list.search-list.articles': '搜索列表（文章）',
+    'menu.zj.list.search-list.projects': '搜索列表（项目）',
+    'menu.zj.list.search-list.applications': '搜索列表（应用）'
 }
