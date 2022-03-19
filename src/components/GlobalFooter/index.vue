faf6a50a53281d853dbee5b97bbea993576e5e6d
lihh1300308284 <1300308284@qq.com>
2022-03-19 11:11:39 +0800

    Update index.vue

--- src/components/GlobalFooter/index.vue
+++ src/components/GlobalFooter/index.vue
@@ -1,7 +1,7 @@
 <template>
   <global-footer class="footer custom-render">
     <template v-slot:links>
-      <a target="_blank">Copyright © 2022-2022 AC. All rights reserved.</a>
+      <a target="_blank">Copyright © 2022-2022<span style="color: #007bff"> AC. </span>All rights reserved.</a>
       <!-- <a href="https://www.github.com/vueComponent/ant-design-vue-pro" target="_blank">Github</a>
       <a href="https://www.github.com/sendya/" target="_blank">@Sendya</a> -->
     </template>
